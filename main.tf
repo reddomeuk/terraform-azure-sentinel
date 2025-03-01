7a8676ef33059bcd62a62fd5ea68ed439e8f980d
reddomeuk <academy@reddome.org>
2025-03-01 12:30:30 +0000

    Fix workspace_id format in sentinel onboarding resource

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ module "labels" {
 
 resource "azurerm_sentinel_log_analytics_workspace_onboarding" "main" {
   count        = var.enabled && var.sentinel_enabled ? 1 : 0
-  workspace_id = data.azurerm_log_analytics_workspace.existing.workspace_id
+  workspace_id = data.azurerm_log_analytics_workspace.existing.id
 }
 
 resource "azurerm_sentinel_alert_rule_ms_security_incident" "main" {
