fdb2c2bfc7eb0384cca60ea92aa6e8f0a667d9d8
reddomeuk <academy@reddome.org>
2025-03-01 12:42:28 +0000

    Remove import configuration after successful import

--- import.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = azurerm_log_analytics_solution.sentinel
-  id = "/subscriptions/54a759cf-d86c-4a94-83b9-f2771d7fced7/resourceGroups/reddome-dev-rg/providers/Microsoft.OperationsManagement/solutions/SecurityInsights(reddome-dev-law)"
-}
